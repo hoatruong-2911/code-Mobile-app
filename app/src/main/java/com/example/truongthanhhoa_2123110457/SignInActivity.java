3701fa6088c8b3e7d86f85a253c3bcf23585bcce
Truong Thanh Hoa <sivankieu@gmail.com>
2025-07-31 08:50:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/truongthanhhoa_2123110457/SignInActivity.java
@@ -0,0 +1,42 @@
+package com.example.truongthanhhoa_2123110457;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_in);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Nút Sign In
+        Button btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(v -> {
+            // Sau khi kiểm tra đăng nhập thành công -> vào HomeActivity
+            Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish(); // đóng SignInActivity để không quay lại khi bấm Back
+        });
+
+        // 🔹 Bắt sự kiện click nút Create New Account
+        Button btnCreateAccount = findViewById(R.id.btnCreateAccount);
+        btnCreateAccount.setOnClickListener(v -> {
+            Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
+    }
+}
